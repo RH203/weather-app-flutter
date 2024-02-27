427b28976ef084ea23f26c6d78907498ef8227a7
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-27 20:24:31 +0700

    feat(update): add page weather

--- lib/src/features/weather/weather_page.dart
+++ lib/src/features/weather/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/src/features/welcome/welcome_page.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,10 +9,115 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  var _nameCity;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("data"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Weather App",
+          style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: SizedBox(
+              width: 380,
+              child: TextField(
+                controller: _nameCity,
+                decoration: InputDecoration(
+                  hintText: "Indonesia",
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Text(
+              "CIty Name",
+              style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+          ),
+          Row(children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 220, 212, 212),
+                    offset: Offset(
+                      3,
+                      3,
+                    ),
+                    blurRadius: 10,
+                    spreadRadius: 2,
+                  ),
+                  BoxShadow(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    offset: Offset(
+                      0,
+                      0,
+                    ),
+                    blurRadius: 0,
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        "18 ",
+                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontSize: 70,
+                            ),
+                      ),
+                      Image.asset(
+                        "assets/images/cloudy-sample.png",
+                        width: 100,
+                      )
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "10",
+                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontSize: 20,
+                            ),
+                      ),
+                      Text(
+                        "200",
+                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontSize: 20,
+                            ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ]),
+        ],
+      ),
     );
   }
 }
