1ab4a3acbe323e75809e25f960d3643607db584f
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-29 22:54:38 +0700

    feat(update): Added new variables for weather data that has been taken from OpenWeatherApi

--- lib/src/models/weather_models.dart
+++ lib/src/models/weather_models.dart
@@ -1,26 +1,39 @@
 class WeatherModels {
-  final String main, description, icon, responStatus;
-  final double temp, tempMin, tempMax, humidity;
+  final String main, description, icon, nameCity;
+  final double temp, tempMin, tempMax, wind;
+  final int deg, sunrise, sunset, responStatus, humidity;
 
-  WeatherModels(
-      {required this.main,
-      required this.description,
-      required this.icon,
-      required this.responStatus,
-      required this.temp,
-      required this.tempMin,
-      required this.tempMax,
-      required this.humidity});
+  WeatherModels({
+    required this.main,
+    required this.nameCity,
+    required this.description,
+    required this.icon,
+    required this.responStatus,
+    required this.temp,
+    required this.tempMin,
+    required this.tempMax,
+    required this.humidity,
+    required this.wind,
+    required this.deg,
+    required this.sunrise,
+    required this.sunset,
+  });
 
   factory WeatherModels.fromJson(Map<String, dynamic> json) {
     return WeatherModels(
-        main: json['weather'][0]['main'],
-        description: json['weather'][0]['description'],
-        icon: json['weather'][0]['icon'],
-        responStatus: json['cod'],
-        temp: json['main']['temp'],
-        tempMin: json['main']['temp_min'],
-        tempMax: json['main']['temp_max'],
-        humidity: json['main']['humidity']);
+      main: json['weather'][0]['main'],
+      nameCity: json['name'],
+      description: json['weather'][0]['description'],
+      icon: json['weather'][0]['icon'],
+      responStatus: json['cod'],
+      temp: json['main']['temp'],
+      tempMin: json['main']['temp_min'],
+      tempMax: json['main']['temp_max'],
+      humidity: json['main']['humidity'],
+      wind: json['wind']['speed'],
+      deg: json['wind']['deg'],
+      sunrise: json['sys']['sunrise'],
+      sunset: json['sys']['sunset'],
+    );
   }
 }
