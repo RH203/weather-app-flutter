9c3dfe16de272ff9ee500da52ae9baa61b32a84c
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-29 15:55:14 +0700

    feat(update): update layout ui weather

--- lib/src/features/weather/weather_page.dart
+++ lib/src/features/weather/weather_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/src/common/card/information_weather_card.dart';
 import 'package:weather_app/src/features/welcome/welcome_page.dart';
 import 'package:weather_icons/weather_icons.dart';
-import 'dart:ui' as ui;
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -12,6 +12,10 @@ class WeatherPage extends StatefulWidget {
 
 class _WeatherPageState extends State<WeatherPage> {
   var _nameCity;
+
+  void _onTapBottomNavigationBar() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +66,10 @@ class _WeatherPageState extends State<WeatherPage> {
                     controller: _nameCity,
                     decoration: InputDecoration(
                       hintText: "Indonesia",
-                      prefixIcon: const Icon(Icons.search),
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.search),
+                      ),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(12),
                       ),
@@ -82,112 +89,156 @@ class _WeatherPageState extends State<WeatherPage> {
                       ),
                 ),
               ),
-              Row(children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0, left: 15.0),
-                  child: SizedBox(
-                    width: 380,
-                    child: Row(
+              Padding(
+                padding: const EdgeInsets.only(left: 8, top: 8),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        SizedBox(
-                          width: 180,
+                        Text(
+                          "Rain Rainn",
+                          style:
+                              Theme.of(context).textTheme.labelLarge!.copyWith(
+                                    color: const Color(0xffFCFCFD),
+                                    fontSize: 45,
+                                  ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15),
+                          child: Text(
+                            "Hujan deras",
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge!
+                                .copyWith(
+                                  color:
+                                      const Color.fromARGB(255, 212, 212, 216),
+                                  fontSize: 20,
+                                ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Image.asset(
+                          "assets/images/cloudy-sample.png",
+                          width: 200,
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "200",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelLarge!
+                                      .copyWith(
+                                        color: const Color(0xffFCFCFD),
+                                        fontSize: 49,
+                                      ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const Padding(
+                                  padding:
+                                      EdgeInsets.only(right: 0, bottom: 25),
+                                  child: Icon(
+                                    WeatherIcons.celsius,
+                                    size: 70,
+                                    color: Color.fromARGB(255, 212, 212, 216),
+                                  ),
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  "200",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelLarge!
+                                      .copyWith(
+                                        color: const Color(0xffFCFCFD),
+                                        fontSize: 49,
+                                      ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const Padding(
+                                  padding:
+                                      EdgeInsets.only(left: 10, bottom: 25),
+                                  child: Icon(
+                                    WeatherIcons.humidity,
+                                    size: 50,
+                                    color: Color.fromARGB(255, 212, 212, 216),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 17, left: 8, right: 8),
+                  child: Container(
+                    width: double.infinity,
+                    height: double.minPositive,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(18),
+                        topRight: Radius.circular(18),
+                      ),
+                    ),
+                    child: const CustomScrollView(
+                      slivers: [
+                        SliverPadding(
+                          padding: EdgeInsets.only(
+                            top: 16,
+                            left: 7,
+                            right: 7,
+                          ),
+                        ),
+                        SliverToBoxAdapter(
                           child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
-                              Text(
-                                "18 ",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .labelLarge!
-                                    .copyWith(
-                                      color: const Color(0xffFCFCFD),
-                                      fontSize: 80,
-                                    ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  InformationWeatherCard(),
+                                  InformationWeatherCard()
+                                ],
                               ),
-                              Image.asset(
-                                "assets/images/cloudy-sample.png",
-                                width: 180,
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  InformationWeatherCard(),
+                                  InformationWeatherCard()
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  InformationWeatherCard(),
+                                  InformationWeatherCard()
+                                ],
                               ),
                             ],
                           ),
                         ),
-                        Expanded(
-                            child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Wind",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge!
-                                      .copyWith(
-                                        color: const Color.fromARGB(
-                                            255, 220, 214, 214),
-                                        fontSize: 20,
-                                      ),
-                                ),
-                                const Padding(
-                                  padding: EdgeInsets.only(left: 30),
-                                  child: Icon(
-                                    Icons.waves,
-                                    color: Color(0xffFCFCFD),
-                                    size: 35,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Text(
-                              "10",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelLarge!
-                                  .copyWith(
-                                    color: const Color(0xffFCFCFD),
-                                    fontSize: 40,
-                                  ),
-                              textAlign: TextAlign.start,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "Humidity",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge!
-                                      .copyWith(
-                                        color: const Color.fromARGB(
-                                            255, 220, 214, 214),
-                                        fontSize: 20,
-                                      ),
-                                ),
-                                const BoxedIcon(
-                                  WeatherIcons.humidity,
-                                  color: Color(0xffFCFCFD),
-                                  size: 35,
-                                ),
-                              ],
-                            ),
-                            Text(
-                              "200",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelLarge!
-                                  .copyWith(
-                                    color: const Color(0xffFCFCFD),
-                                    fontSize: 40,
-                                  ),
-                            ),
-                          ],
-                        )),
                       ],
                     ),
                   ),
                 ),
-              ]),
+              ),
             ],
           ),
         ),
