4772b2663a3b849aa570b006ca30f503c8fce4fa
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-29 22:45:52 +0700

    feat(update): setting app as root app for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 import 'package:weather_app/src/app.dart';
 
 import 'package:weather_app/src/provider/weather_provider.dart';
 
 Future main() async {
   await dotenv.load(fileName: "lib/.env");
-  runApp(const App());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => WeatherProvider(),
+      child: const App(),
+    ),
+  );
 }
