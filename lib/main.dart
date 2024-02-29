36d06c4712a0d04c4c5cd686185588cad7ee3ac7
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-29 17:50:50 +0700

    feat(update): add new packages

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather_app/src/app.dart';
 
+import 'package:weather_app/src/provider/weather_provider.dart';
+
 Future main() async {
   await dotenv.load(fileName: "lib/.env");
   runApp(const App());
