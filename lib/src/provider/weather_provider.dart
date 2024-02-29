5eaeeb41d6f971d56666de2c854a2f7c4f269830
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-29 15:56:45 +0700

    feat(update): add new packages

--- lib/src/provider/weather_provider.dart
+++ lib/src/provider/weather_provider.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:weather_app/src/models/weather_models.dart';
+import 'package:http/http.dart' as http;
 
-class WeatherProvider extends ChangeNotifier {}
+class WeatherProvider extends ChangeNotifier {
+  late WeatherModels _currentWeather;
+  WeatherModels get currentWeather => _currentWeather;
+
+  Future<void> fetchWeather(String cityName) async {
+    final url =
+        "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${dotenv.env["API_KEY_WEATHER"]}";
+
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      _currentWeather = WeatherModels.fromJson(jsonData);
+      notifyListeners();
+    } else {
+      throw Exception('Failed to load weather data.');
+    }
+  }
+}
