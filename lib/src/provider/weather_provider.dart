3d3b6ec0cf4dd274335b3f187e3dbae07eea02f0
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-29 22:55:40 +0700

    feat(update): Added debugging code and also added try catch

--- lib/src/provider/weather_provider.dart
+++ lib/src/provider/weather_provider.dart
@@ -5,20 +5,25 @@ import 'package:weather_app/src/models/weather_models.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherProvider extends ChangeNotifier {
-  late WeatherModels _currentWeather;
-  WeatherModels get currentWeather => _currentWeather;
+  WeatherModels? _currentWeather;
+
+  WeatherModels? get currentWeather => _currentWeather;
 
   Future<void> fetchWeather(String cityName) async {
-    final url =
-        "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${dotenv.env["API_KEY_WEATHER"]}";
+    try {
+      final url =
+          "https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${dotenv.env["API_KEY_WEATHER"]}";
 
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      _currentWeather = WeatherModels.fromJson(jsonData);
-      notifyListeners();
-    } else {
-      throw Exception('Failed to load weather data.');
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        _currentWeather = WeatherModels.fromJson(jsonData);
+        notifyListeners();
+      } else {
+        throw Exception('Failed to load weather data.');
+      }
+    } catch (e) {
+      print("Error PROVIDER: $e");
     }
   }
 }
